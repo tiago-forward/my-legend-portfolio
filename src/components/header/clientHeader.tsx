b614126e978f8d505fe9f12260493de2ba1b2e1d
tiagoforward <devtiagoforward@gmail.com>
2024-08-17 18:03:32 -0300

    componentized header and adjusted the responsive

---
+++ src/components/header/clientHeader.tsx
@@ -0,0 +1,83 @@
+import { Separator } from '@radix-ui/react-separator'
+import React from 'react'
+
+import Logo from '@/assets/logo.png'
+
+import { NavLinkHeader } from '../navLinks/navLinkHeader'
+
+export function ClientHeader() {
+  const primaryNavItems = [{ id: 1, href: '/', label: 'Início' }]
+
+  const secondaryNavItems = [
+    { id: 1, href: '/sobre', label: 'Sobre' },
+    { id: 2, href: '/habilidades', label: 'Habilidades' },
+    { id: 3, href: '/formacoes', label: 'Formações' },
+    { id: 4, href: '/projetos', label: 'Projetos' },
+  ]
+
+  return (
+    <header className="border-border-header border-t-2">
+      <nav className="grid grid-cols-1 lg:grid-cols-5 lg:gap-4">
+        <ul className="mt-8 flex flex-col items-center uppercase lg:col-span-2 lg:mt-0 lg:flex-row lg:pl-8">
+          <li className="">
+            <img src={Logo} alt="" className="w-[350px] p-1" />
+          </li>
+          {primaryNavItems.map((item) => (
+            <>
+              <li className="flex h-full items-center">
+                <NavLinkHeader to={item.href}>{item.label}</NavLinkHeader>
+              </li>
+            </>
+          ))}
+        </ul>
+
+        <ul className="flex items-center justify-center uppercase lg:col-span-2 lg:justify-start lg:pr-8">
+          {secondaryNavItems.map((item, index) => (
+            <React.Fragment key={item.href}>
+              <li className="flex h-full items-center">
+                <NavLinkHeader to={item.href}>{item.label}</NavLinkHeader>
+              </li>
+              {(index + 1) % 2 === 0 &&
+                index < secondaryNavItems.length - 1 && (
+                  <Separator
+                    orientation="vertical"
+                    className="user-select-none center pointer-events-none h-16 w-[2px] transform-gpu [background:linear-gradient(180deg,rgba(0,0,0,0)_0%,rgba(199,160,0,0.65)_50%,rgba(0,0,0,0)_100%)]"
+                  />
+                )}
+            </React.Fragment>
+          ))}
+        </ul>
+
+        <div className="bg-aside-bg flex h-full items-center gap-4 px-3 pb-2 lg:col-span-1">
+          <div className="relative my-2 w-16 rounded-full border border-black hover:text-white">
+            <img
+              src="https://github.com/tiago-forward.png"
+              alt="tiago forward"
+              className="rounded-full border-4 border-[#e9b874]"
+            />
+            <div className="flex items-center justify-center">
+              <span
+                className="absolute cursor-default rounded-[6px] border-2 border-[#e9b874] bg-[#2f3640] px-2 text-xs font-bold"
+                title="Idade"
+              >
+                26
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <span
+              className="cursor-default font-bold text-[#f1ede1]"
+              title="Nome"
+            >
+              TiagoForward
+            </span>
+            <div className="flex items-center gap-2">
+              <div className="bola bg-green-500"></div>
+              <span className="mt-1 cursor-default text-green-500">Online</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
--- src/components/navLinks/navLinkHeader.tsx
+++ src/components/navLinks/navLinkHeader.tsx
@@ -8,7 +8,7 @@ export function NavLinkHeader(props: NavLinkHeaderProps) {
   return (
     <Link
       data-current={pathname === props.to}
-      className="hover:bg-navLinkHover data-[current=true]:bg-navLinkHover flex h-full items-center px-4 text-sm transition duration-300 hover:text-[#f1ede1] data-[current=true]:text-[#f1ede1]"
+      className="hover:bg-navLinkHover data-[current=true]:bg-navLinkHover flex h-full items-center px-2 text-sm transition duration-300 hover:text-[#f1ede1] data-[current=true]:text-[#f1ede1] lg:text-base xl:px-4"
       {...props}
     />
   )
