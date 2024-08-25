2ce2e12ee3a4f08d94b1accf0f0fdf5f8ec2d635
tiagoforward <devtiagoforward@gmail.com>
2024-08-25 07:35:00 -0300

    adjustment in the start component

--- src/pages/app/inicio/clientGeral.tsx
+++ src/pages/app/inicio/clientGeral.tsx
@@ -23,7 +23,7 @@ export function ClientGeral() {
           <h1 className="text-5xl font-bold uppercase text-amber-50">
             Seja bem vindo ao meu novo portfolio
           </h1>
-          <p className="text-xl text-[#efe8d186]">Desenvolvedor front-end</p>
+          <h2 className="text-xl text-[#efe8d186]">Desenvolvedor front-end</h2>
           <Button asChild className="buttonGeral group rounded-none">
             <a
               href="#"
